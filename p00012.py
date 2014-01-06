6f96a4dd00fccad96829412e8d0c90fba9d33f8d
z07 <jklasdf35@gmail.com>
2014-01-06 16:25:23 +0900

    p00012.py

--- p00012.py
+++ p00012.py
@@ -1,19 +1,20 @@
 import sys
+from math import *
 from time import time
 start = time()
 n_list = []
-for i in range(2,10000):
+for i in range(10000,15000):
     n_list.append(sum(range(1,i)))
 
 d_list_list = []
 for i in range(len(n_list)):
     d_list = []
-    for j in range(n_list[i]):
-        if n_list[i]%(n_list[i]-j) == 0:
-            d_list.append(n_list[i] - j)
+    for j in range(1,int(sqrt(n_list[i]))):
+        if n_list[i]%j == 0:
+            d_list.append(j)
     d_list_list.append(d_list)
-    if len(d_list_list[-1]) >= 500:
-        print n_list[i],d_list, len(d_list)
+    if len(d_list_list[-1])*2 >= 500:
+        print n_list[i],len(d_list)
         sys.exit()
 
 print "1 : Seconds", time() - start
